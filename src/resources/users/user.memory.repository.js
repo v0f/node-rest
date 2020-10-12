49dec9abe2ec021a6f3dc6b1d258266f93925cd9
vof <vof1111@gmail.com>
2020-10-12 06:00:02 +0300

    feat: implement /users rest endpoint

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,30 @@
+const db = require('../../common/db');
+const _ = require('lodash');
+const User = require('./user.model');
+
 const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+  return db.Users;
 };
 
-module.exports = { getAll };
+const get = async id => {
+  const user = _.find(db.Users, ['id', id]);
+  return _.omit(user, 'password');
+};
+
+const put = async (id, put_data) => {
+  const user = _.find(db.Users, ['id', id]);
+  _.assign(user, put_data);
+  return _.omit(user, 'password');
+};
+
+const post = async post_data => {
+  const user = new User(post_data);
+  db.Users.push(user);
+  return _.omit(user, 'password');
+};
+
+const user_delete = async id => {
+  _.remove(db.Users, e => e.id === id);
+};
+
+module.exports = { getAll, get, post, put, user_delete };
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,9 @@
 const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
+const get = id => usersRepo.get(id);
+const post = post_data => usersRepo.post(post_data);
+const put = (id, put_data) => usersRepo.put(id, put_data);
+const user_delete = id => usersRepo.user_delete(id);
 
-module.exports = { getAll };
+module.exports = { getAll, get, post, put, user_delete };
